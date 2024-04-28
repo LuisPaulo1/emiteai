51a7ed65b5be622282aa65fde2cfe72fe3ace7b0
LuisPaulo1 <csluispaulo@gmail.com>
2024-04-28 12:33:02 -0300

    implementação model

---
+++ backend/src/main/java/com/emiteai/model/Endereco.java
@@ -0,0 +1,49 @@
+package com.emiteai.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Endereco {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 20)
+    private String numero;
+
+    @Column(length = 100)
+    private String complemento;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false, length = 100)
+    private String bairro;
+
+    @Column(nullable = false, length = 100)
+    private String municipio;
+
+    @Column(nullable = false, length = 100)
+    private String estado;
+
+    @OneToMany(mappedBy = "endereco")
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+}
---
+++ backend/src/main/java/com/emiteai/model/Pessoa.java
@@ -0,0 +1,41 @@
+package com.emiteai.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pessoa {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 30)
+    private String telefone;
+
+    @CPF
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_id", foreignKey = @ForeignKey(name = "fk_pessoa_endereco"))
+    private Endereco endereco;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+}
