64b119b4cd5146b45becc62510fb89e56b1a6810
LuisPaulo1 <csluispaulo@gmail.com>
2024-04-28 11:28:52 -0300

    first commit

---
+++ backend/src/main/java/com/emiteai/EmiteaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.emiteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiteaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiteaiApiApplication.class, args);
+	}
+
+}
