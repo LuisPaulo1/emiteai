fef21eafeba07823447fb5f74d679c44c0adbc23
LuisPaulo1 <csluispaulo@gmail.com>
2024-04-28 12:37:08 -0300

    Implementação repository

---
+++ backend/src/main/java/com/emiteai/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.emiteai.repository;
+
+import com.emiteai.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+}
